436706dbf8b0c12c2060afc1dd6ded617ea3a5f5
James Stubbington <teanbiskuts@gmail.com>
2026-02-12 08:42:16 -0600

    chore: fix dashboard header name

--- src/Winnow.Client/src/App.tsx
+++ src/Winnow.Client/src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <div className="w-[1px] h-4 bg-border mx-2" />
-          <span className="font-medium">Cluster Dashboard</span>
+          <span className="font-medium">Winnow Triage</span>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
           <Routes>
