abdd8a784e8b81aa75a37db39fc670acd6178803
James Stubbington <teanbiskuts@gmail.com>
2026-03-04 07:24:16 -0600

    feat: Add AssignedTo column to the Clusters table in postgres.

---
+++ src/Services/Winnow.Server/Migrations/Postgres/20260304132010_AddClusterAssignmentPg.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Winnow.Server.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class AddClusterAssignmentPg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedTo",
+                table: "Clusters",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedTo",
+                table: "Clusters");
+        }
+    }
+}
