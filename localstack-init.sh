8149700137602e2782b81ad653ddf026914b2ed0
James Stubbington <teanbiskuts@gmail.com>
2026-03-24 17:14:20 -0500

    feat: Initialize `winnow-security-defs` S3 bucket in LocalStack setup.

--- localstack-init.sh
+++ localstack-init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Initializing LocalStack resources..."
 
-# Create S3 buckets
+# Create S3 buckets for asset scanning
 awslocal s3 mb s3://winnow-quarantine
 awslocal s3 mb s3://winnow-clean
+
+# Create S3 bucket for security definitions
+awslocal s3 mb s3://winnow-security-defs
 
 echo "LocalStack initialization complete!"
