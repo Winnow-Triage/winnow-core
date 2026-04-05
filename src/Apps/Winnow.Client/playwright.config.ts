7759be6d331c331792a5ef1b28142b0b6c6497f3
James Stubbington <teanbiskuts@gmail.com>
2026-04-05 09:11:18 -0500

    ci: integrate GitHub Actions test reporting and publish results to pull requests

--- src/Apps/Winnow.Client/playwright.config.ts
+++ src/Apps/Winnow.Client/playwright.config.ts
@@ -15,7 +15,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI or locally for service stability. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI 
+    ? [['html'], ['junit', { outputFile: 'playwright-report/results.xml' }], ['github']] 
+    : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
